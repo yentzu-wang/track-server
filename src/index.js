f4afb1b307647a6f36cb726646399dfd0dc4b582
Frank <frank7300228@gmail.com>
2020-05-19 17:20:43 +0800

    Finish 14-168

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 const express = require("express")
+const mongoose = require("mongoose")
 
 const app = express()
+const mongoUri =
+  "mongodb+srv://admin:passwordpassword@cluster0-9tmx8.mongodb.net/test?retryWrites=true&w=majority"
+
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true
+})
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongo instance")
+})
+
+mongoose.connection.on("error", err => {
+  console.log("Error connecting to mongo", err)
+})
 
 app.get("/", (req, res) => {
   res.send("Hi there!")
